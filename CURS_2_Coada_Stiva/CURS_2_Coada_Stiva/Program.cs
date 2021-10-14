6710dddfd7e16615e8beed45effb27426e04c09d
Daria Trip <daria.trip001@gmail.com>
2021-10-14 16:11:06 +0300

    added queue

---
+++ CURS_2_Coada_Stiva/CURS_2_Coada_Stiva/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURS_2_Coada_Stiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myqueue = new Queue();
+
+            Console.WriteLine("Type the queue lenght: ");
+            int queue_lenght = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insert elements of the queue:");
+            for (int i=0;i<queue_lenght;i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine()); 
+                myqueue.Push(number);
+            }
+
+            Console.WriteLine();
+            myqueue.View();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Type 1 for adding new elements \n Type 0 to delete elements." +
+                "\n To end the process type any key (different from 1 or 0)");
+
+            int action = Convert.ToInt32(Console.ReadLine());
+            while(action==0 || action==1)
+            {
+                if(action==1)
+                {
+                    Console.WriteLine("Insert the new element: ");
+                    int new_element = Convert.ToInt32(Console.ReadLine());
+                    myqueue.Push(new_element);
+                    myqueue.View();
+                }
+                else
+                {
+                    myqueue.Pop();
+                    myqueue.View();
+                }
+            }
+
+        }
+    }
+}
---
+++ CURS_2_Coada_Stiva/CURS_2_Coada_Stiva/Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURS_2_Coada_Stiva
+{
+    class Queue
+    {
+        int[] values;
+
+        public Queue()
+        {
+            values = new int[0];
+        }
+
+        public void Push(int ValueToAdd)
+        {
+            int[] temp = new int[values.Length + 1];
+            for(int i = 0; i < values.Length; i++)
+            {
+                temp[i + 1] = values[i];
+            }
+            temp[0] = ValueToAdd;
+            values = temp;
+        }
+
+        public int Pop()
+        {
+            int toReturn = values[values.Length - 1];
+            int[] temp = new int[values.Length - 1];
+            for(int i = 0; i < values.Length - 1; i++)
+            {
+                temp[i] = values[i];
+            }
+            values = temp;
+            return toReturn;
+        }
+
+        public void View()
+        {
+            foreach(int value in values)
+            {
+                Console.Write(value + " ");
+            }
+        }
+    }
+}
